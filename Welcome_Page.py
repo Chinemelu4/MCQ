b8256a23228f3b26a5778d8e0997ee8a20082269
Ezinwanne Aka <ezinwanneaka@gmail.com>
2024-08-15 00:58:44 +0100

    Create Welcome_Page.py

---
+++ Welcome_Page.py
@@ -0,0 +1,17 @@
+import streamlit as st
+from PIL import Image
+
+st.set_page_config(
+    page_title="Hello",
+    page_icon="👋",
+)
+
+st.write("# Welcome My MCQ Validation Site! 👋")
+
+st.sidebar.success("Select a Scenario")
+
+st.markdown(
+    """
+    Select a scenario for the questions you want to validate!!!
+"""
+)
