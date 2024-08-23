0fb34c7971bcfc32cb66ef1f9f22495a66b4338f
Ezinwanne Aka <ezinwanneaka@gmail.com>
2024-08-23 21:26:36 +0100

    Update 2 African Expertise.py

--- pages/2 African Expertise.py
+++ pages/2 African Expertise.py
@@ -61,7 +61,8 @@ if len(filtered_df) > 30:
 @st.cache
 def convert_df_to_csv(df):
     return df.to_csv(index=False).encode('utf-8')
-
+    
+st.write("Please Click the download button to download the questions")
 csv = convert_df_to_csv(filtered_df)
 st.download_button(
     label="Download CSV",
