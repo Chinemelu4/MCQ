40214a2e0cf2e32f5bcc2e814ba0a0f65d82c3fe
Ezinwanne Aka <ezinwanneaka@gmail.com>
2024-10-05 12:11:49 +0100

    Add files via upload

--- Welcome_Page.py
+++ Welcome_Page.py
@@ -6,12 +6,33 @@ st.set_page_config(
     page_icon="👋",
 )
 
-st.write("# MCQ Validation Site! 👋")
-
-st.sidebar.success("Select a Scenario")
+st.write("# Welcome To The MCQ Validation Site! 👋")
 
 st.markdown(
     """
-    Select a scenario for the questions you want to validate!!!
+    ## Evaluation Instructions
+
+### To begin your evaluation, please follow these steps:
+
+#### 1. Select Your Question Type of Interest for Evaluation:
+- **Questions from a specific Specialty.**
+- **Questions requiring African Expertise.**
+- **Questions with LLM responses marked as having Potential for Bias.**
+- **Questions with LLM responses marked as having Potential for Harm.**
+
+#### 2. Generate Questions:
+- Create a CSV file containing randomized questions from the **AfriMed-QA** dataset that meet your selected criteria.
+
+#### 3. Review Questions and Model Responses:
+- Download your CSV file.
+- Carefully review the questions and the corresponding model responses, using the provided feedback form as a guide.
+
+#### 4. Submit Feedback:
+- Complete the feedback survey based on the set of 30 questions you reviewed in step 1.
+
+---
+
+Thank you for your valuable contribution to advancing healthcare research in Africa!
+
 """
 )
